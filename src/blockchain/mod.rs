58d03e1401d25240dfc98c5d7b24e675b27dc513
Chief Se'khu Hadjo <micohadjo@gmail.com>
2024-09-17 11:50:29 -0400

    Update mod.rs

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -1,35 +1,42 @@
 use std::collections::HashMap;
 use sha2::{Sha256, Digest};
 use serde::{Serialize, Deserialize};
-use quantum_sig::QuantumSig;  // Using the futuristic quantum-resistant signature scheme
+use ed25519_dalek::{Keypair, Signature, Signer, PUBLIC_KEY_LENGTH, SIGNATURE_LENGTH};
+use rand::rngs::OsRng;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Block {
     pub hash: String,
     pub previous_hashes: Vec<String>,
     pub data: String,
-    pub signature: String,  // Each block contains a quantum-resistant signature
+    pub signature: Vec<u8>,
 }
 
 #[derive(Clone)]
 pub struct N8IVChain {
     pub blocks: HashMap<String, Block>,
     pub genesis_block: Block,
+    pub keypair: Keypair, // Add keypair for signing blocks
 }
 
 impl N8IVChain {
     pub fn new() -> Self {
+        // Generate a keypair for signing the blocks
+        let mut csprng = OsRng{};
+        let keypair = Keypair::generate(&mut csprng);
+
         let genesis_block = Block {
             hash: String::from("genesis"),
             previous_hashes: vec![],
             data: String::from("Genesis Block"),
-            signature: QuantumSig::generate_signature("Genesis Block").unwrap(),
+            signature: vec![0; SIGNATURE_LENGTH], // Placeholder signature
         };
         let mut blocks = HashMap::new();
         blocks.insert(genesis_block.hash.clone(), genesis_block.clone());
         Self {
             blocks,
             genesis_block,
+            keypair,
         }
     }
 
@@ -39,7 +46,10 @@ impl N8IVChain {
 
     pub fn add_block(&mut self, previous_hashes: Vec<String>, data: String) {
         let hash = self.generate_hash(&data);
-        let signature = QuantumSig::generate_signature(&data).unwrap();
+
+        // Sign the data using the keypair
+        let signature = self.keypair.sign(hash.as_bytes()).to_bytes().to_vec();
+
         let new_block = Block {
             hash,
             previous_hashes,
@@ -48,6 +58,12 @@ impl N8IVChain {
         };
         self.blocks.insert(new_block.hash.clone(), new_block);
     }
+
+    fn generate_hash(&self, data: &str) -> String {
+        format!("{:x}", Sha256::digest(data.as_bytes()))
+    }
+}
+
 
     fn generate_hash(&self, data: &str) -> String {
         format!("{:x}", Sha256::digest(data.as_bytes()))
